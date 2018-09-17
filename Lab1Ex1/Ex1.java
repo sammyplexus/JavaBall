26caad1e5caef4046a46a43b8f8c354041b1e5be
Samuel Agbede <sammyplexus@gmail.com>
2018-09-17 16:52:09 +0100

    Made naming modification to TeamsIn

--- Lab1Ex1/Ex1.java
+++ Lab1Ex1/Ex1.java
@@ -1,8 +1,26 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 class Ex1 {
 
-	public static void main(String[] args) {
-		System.err.println("Hello World");
+	public static void main(String[] args) {		
+		try {
+			FileReader reader = new FileReader("TeamsIn.txt");
+			Scanner scanner = new Scanner(reader);
+			
+			while (scanner.hasNextLine()) {
+				System.out.println(scanner.nextLine());
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
